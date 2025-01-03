ebffe3a339f9ef985a32ac16f984aaa289662aea
Fokko Veegens <24793348+FokkoVeegens@users.noreply.github.com>
2025-01-03 14:31:10 +0100

    Implement hash-based cache busting for data fetch

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,15 +1,23 @@
 import basename from "../../react-config";
+import CryptoJS from 'crypto-js';
 
 let cachedData = null;
 
 const getData = async () => {
-    const cacheBuster = new Date().getTime(); // Generate a unique timestamp
     try {
-        const response = await fetch(`${basename}data.json?cacheBuster=${cacheBuster}`);
+        const response = await fetch(`${basename}data.json`);
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-        cachedData = await response.json();
+        const fileBuffer = await response.text(); // Read file as a string
+        const hashSum = CryptoJS.SHA256(fileBuffer);
+        const cacheBuster = hashSum.toString(CryptoJS.enc.Hex); // Generate a hash of the file
+
+        const cacheBustedResponse = await fetch(`${basename}data.json?cacheBuster=${cacheBuster}`);
+        if (!cacheBustedResponse.ok) {
+            throw new Error('Network response was not ok');
+        }
+        cachedData = await cacheBustedResponse.json();
         return cachedData;
     } catch (error) {
         console.error('Fetch error:', error);
